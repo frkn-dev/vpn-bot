a873486cbc99fabc2a12c7f16b0728d41b5a8bdb
zapolnoch <zapolnoch@gmail.com>
2025-08-13 21:55:27 +0300

    connectWithMnemonic

---
+++ src/site/types.ts
@@ -0,0 +1,31 @@
+interface ErrorResponse {
+  status: "error";
+  message: string;
+  cause?: {
+    name: string;
+    code?: string;
+    issues?: [];
+  };
+}
+
+interface SuccessResponse {
+  status: "success";
+  token: string;
+}
+
+export type LoginResponse = ErrorResponse | SuccessResponse;
+
+export type ActiveConnection = {
+  status: "active";
+  subscription_url: string;
+  ss_links: {
+    country: string;
+    link: string;
+  }[];
+  used_traffic: number;
+  limit: number | null;
+  limit_reset_strategy: string;
+  created_at: string;
+  expire: string | null;
+  online_at: string | null;
+};
